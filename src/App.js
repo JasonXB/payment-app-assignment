b9bf676a2ba6af427c93a9b8d179aa8cf62e77b1
JasonXB <jasonxportfolio@gmail.com>
2023-01-05 11:38:42 -0500

    Removed boiler plate from App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
-import logo from "./logo.svg";
-import { useCustomContext } from "./state-management/context";
-import "./App.css";
+import React from "react";
 
 function App() {
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo hidden" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">Placeholder text</div>;
 }
 
 export default App;
