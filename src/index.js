1ddfd0df626c61bb301e100f9335276a1ab41b9a
JasonXB <jasonxportfolio@gmail.com>
2023-01-05 12:34:32 -0500

    NPM Install Tailwind and UUID

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./global.css";
-import App from "./App";
+import App from "./pages/App";
 import GlobalContextProvider from "./state-management/context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
