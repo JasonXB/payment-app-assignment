ae07d6e8b30741d40b1fffb5fea4fa01fc40fd69
JasonXB <jasonxportfolio@gmail.com>
2023-01-07 15:42:08 -0500

    Create Send Payment Button Component

---
+++ src/components/SendPaymentButton.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function SendPaymentButton() {
+  return (
+    <button
+      type="button"
+      class="px-8 pt-2.5 pb-2 bg-blue text-white font-medium text-xs leading-normal uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out flex align-center"
+    >
+      <span className="mr-2 text-white">Send Payment</span>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="white"
+        className="w-3 bi bi-send-fill"
+        viewBox="0 0 16 16"
+      >
+        <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083l6-15Zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471-.47 1.178Z" />
+      </svg>
+    </button>
+  );
+}
