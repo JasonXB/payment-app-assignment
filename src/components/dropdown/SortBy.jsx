c0d11b3784296a6085f0779965222813c7bf944c
JasonXB <jasonxportfolio@gmail.com>
2023-01-08 13:08:05 -0500

    Change how drop down menus hide and reveal content

--- src/components/dropdown/SortBy.jsx
+++ src/components/dropdown/SortBy.jsx
@@ -3,13 +3,20 @@ import DownArrow from "../icons/DownArrow";
 import RightArrow from "../icons/RightArrow";
 
 export default function SortBy() {
-  const btnStyles =
-    "dropdown-item text-sm text-left py-4 px-6 w-full font-normal block whitespace-nowrap bg-transparent hover:bg-lightGray";
+  // Control the nested drop down menus opening and closing with useState
+  const [tray1, setTray1] = React.useState(false);
+  const [tray2, setTray2] = React.useState(false);
+  const toggleTray1 = () => setTray1(!tray1);
+  const toggleTray2 = () => setTray1(!tray2);
+
+  const btnStyles = "dropdown-item text-sm text-left py-4 px-6 w-full font-normal block whitespace-nowrap bg-transparent hover:bg-lightGray"; // prettier-ignore
   // #0b2c53
   return (
     <div className="dropdown relative">
       <button
-        className="dropdownPadding dropdown-toggle bg-blue font-medium text-xs leading-tight rounded flex items-center whitespace-nowrap"
+        className={
+          "dropdownPadding dropdown-toggle bg-blue font-medium text-xs leading-tight rounded flex items-center whitespace-nowrap"
+        }
         type="button"
         id="dropdownMenuButton1"
         data-bs-toggle="dropdown"
@@ -19,7 +26,7 @@ export default function SortBy() {
         <DownArrow />
       </button>
       <ul
-        className="dropdown-menu divide-lightGray min-w-max absolute hidden bg-white text-base z-50 float-left list-none text-left rounded-lg shadow-lg mt-1 m-0 bg-clip-padding border-none"
+        className="hidden dropdown-menu divide-lightGray min-w-max absolute bg-white text-base z-50 float-left list-none text-left rounded-lg shadow-lg mt-1 m-0 bg-clip-padding border-none"
         aria-labelledby="dropdownMenuButton1"
       >
         <li className="w-52">
