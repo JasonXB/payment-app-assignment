0f52af161fb2cae59f4d413d4f1d20a4417d1a23
JasonXB <jasonxportfolio@gmail.com>
2023-01-09 18:06:05 -0500

    Create empty webpage section components

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import LayoutBlock from "../components/LayoutBlock";
-import ExportButton from "../components/buttons/ExportButton";
+import PayoutsOverview from "../pageSections/PayoutsOverview";
+import TableTop from "../pageSections/TableTop";
+import Table from "../pageSections/Table";
 function App() {
   return (
     <LayoutBlock>
-      <ExportButton />
+      <PayoutsOverview />
+      {/* <TableTop />
+      <Table /> */}
     </LayoutBlock>
   );
 }
