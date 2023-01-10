08fb49a236d834a4e7adfb1afb325c39273a1074
JasonXB <jasonxportfolio@gmail.com>
2023-01-10 14:45:52 -0500

    Edit Tabs borders to match the ones in the Table Rows

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -4,11 +4,11 @@ export default function Tabs() {
   return (
     <div className="grid grid-cols-[auto_1fr] mb-8">
       <div>
-        <button className="px-4 py-2 bg-white border-lightGray border-[2px] border-b-0 rounded-t-md">
+        <button className="px-4 py-2 bg-white border-gray border-[1px] border-b-0 rounded-t-md">
           Pending Payouts
         </button>
       </div>
-      <div className="border-b-2 border-lightGray">
+      <div className="border-b-[1px] border-gray">
         <button className="px-4 py-2 text-blue rounded-t-md">
           Paid Payouts
         </button>
