b2862a0d79d03f0d55e2782115478caca6bf40fc
JasonXB <jasonxportfolio@gmail.com>
2023-01-10 13:33:00 -0500

    Remove shadows from Buttons

--- src/components/buttons/buildingBlocks/ButtonBuilder.jsx
+++ src/components/buttons/buildingBlocks/ButtonBuilder.jsx
@@ -6,7 +6,7 @@ export default function ButtonBuilder({ text, className = "" }) {
       type="button"
       className={
         className +
-        " inline-block px-4 py-2.5 bg-blue font-medium text-xs leading-tight rounded shadow-md hover:bg-blue-700 hover:shadow-lg"
+        " inline-block px-4 py-2.5 bg-blue font-medium text-xs leading-tight rounded"
       }
     >
       <span className="text-white">{text}</span>
