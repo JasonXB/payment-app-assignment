1ef75ccb850b7e5205d4393f7ae7ce7564dcd254
JasonXB <jasonxportfolio@gmail.com>
2023-01-11 14:29:40 -0500

    Write Unit Test for payout string conversion method

---
+++ src/utility/payoutFormat.test.js
@@ -0,0 +1,12 @@
+import { numericString } from "./payoutFormat";
+
+describe("test method's ability to round numbers up to 2 decimal-point strings", () => {
+  it("should turn 1.5 into '1.50' instead", () => {
+    const result = numericString(1.5);
+    expect(result).toBe("1.50");
+  });
+  it("should turn 0 into '0.00' instead", () => {
+    const result = numericString(0);
+    expect(result).toBe("0.00");
+  });
+});
