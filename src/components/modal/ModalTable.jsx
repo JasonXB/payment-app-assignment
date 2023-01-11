9610649a1fea40107be2db71496841550ddccfcf
JasonXB <jasonxportfolio@gmail.com>
2023-01-10 22:59:52 -0500

    Add store credit/cash/final totals to Modal table

--- src/components/modal/ModalTable.jsx
+++ src/components/modal/ModalTable.jsx
@@ -4,6 +4,7 @@ import SendPayoutsButton from "../buttons/SendPayouts";
 import { v4 as uuidv4 } from "uuid";
 
 export default function ModalTable({ data }) {
+  const tdStyle = "text-sm text-gray-900 font-medium pl-6 py-4 whitespace-nowrap "; // prettier-ignore
   return (
     <div id="payments-table" className="flex flex-col">
       <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
@@ -36,6 +37,25 @@ export default function ModalTable({ data }) {
                 {data.map((el) => (
                   <ModalTableRow key={uuidv4()} data={el} />
                 ))}
+                <tr className="border-b">
+                  <td className={tdStyle}>Store Credit:</td>
+                  <td className={tdStyle + "font-semibold"}></td>
+                  <td className={tdStyle + "font-normal"}>
+                    ADD UP STORE CREDIT
+                  </td>
+                </tr>
+                <tr className="border-b">
+                  <td className={tdStyle}>Cash:</td>
+                  <td className={tdStyle + "font-semibold"}></td>
+                  <td className={tdStyle + "font-normal"}>ADD UP CASH</td>
+                </tr>
+                <tr className="border-b">
+                  <td className={tdStyle}>Total:</td>
+                  <td className={tdStyle + "font-semibold"}></td>
+                  <td className={tdStyle + "font-normal"}>
+                    SUM OF BOTH TOTALS
+                  </td>
+                </tr>
               </tbody>
             </table>
             <SendPayoutsButton />
