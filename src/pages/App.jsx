f1c18d7318c6d2c1c3972074fffec1e3f3159f3d
JasonXB <jasonxportfolio@gmail.com>
2023-01-10 19:27:10 -0500

    Make modal scroll independently from webpage if it holds enough content

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -10,7 +10,7 @@ import { mockData } from "../mockData";
 function App() {
   return (
     <LayoutBlock>
-      <PayoutModal/>
+      <PayoutModal />
       {/* <PayoutsOverview />
       <TableTop />
       <Table data={mockData} /> */}
